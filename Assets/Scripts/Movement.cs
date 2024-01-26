e3a82ec38e779955d7e6ff64247ff3445cf99f43
JiniferSim <evsaltsina@gmail.com>
2024-01-26 14:41:39 +0100

    spaceship movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
-    public float frequency = 1.0f;
-    public float tilte = 30f;
+    //public float frequency = 1.0f;
+    //public float tilte = 30f;
     public float rotationSpeed = 3f;
 
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        float turnInput = Input.GetAxis("Horizontal");
+
+        float rotationAmount = turnInput * rotationSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+        transform.rotation *= turnRotation;
     }
 }
