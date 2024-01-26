9ee7b58484075b93c4f8ddde55de7cdf1acf70b7
JiniferSim <evsaltsina@gmail.com>
2024-01-26 15:37:35 +0100

    movement and decorations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
-    //public float frequency = 1.0f;
-    //public float tilte = 30f;
     public float rotationSpeed = 3f;
+    public float verticalRotationSpeed = 30f;
+
+    private float currentSpeed = 0f;
 
     void Update()
     {
+        float verticalInput = Input.GetAxis("Vertical");
+        float verticalRotationAmount = verticalInput * verticalRotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.right, verticalRotationAmount);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         float turnInput = Input.GetAxis("Horizontal");
 
