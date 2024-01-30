fe0e08e3a6beaf009b08af1d73559cb0b925b0b8
JiniferSim <evsaltsina@gmail.com>
2024-01-30 10:14:03 +0100

    follow camera rotation

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,20 @@ public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public Transform target;
+    private Quaternion initialRotation;
     private Vector3 initialOffset;
 
     void Start()
     {
+        initialRotation = Quaternion.Inverse(target.rotation) * transform.rotation;
         initialOffset = transform.position - target.position;
     }
 
     void Update()
     {
+        Quaternion targetRotation = target.rotation * initialRotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, FollowSpeed * Time.deltaTime);
+
         Vector3 targetPosition = target.position + initialOffset;
         transform.position = Vector3.Lerp(transform.position, targetPosition, FollowSpeed * Time.deltaTime);
     }
